09630c3d4b18344e20523a0de1ff3fad65a95e06
Francisco Terra <francisco.m.terra@inesctec.pt>
2026-07-31 11:05:20 +0100

    Improve clarity of flash usage and as7341 bug fix

--- software/drivers/include/rp_agrolib_flash.h
+++ software/drivers/include/rp_agrolib_flash.h
@@ -1,27 +1,26 @@
 #pragma once
 
 #include <stdio.h>
-
 #include "pico/stdlib.h"
 extern "C" {
-#include "hardware/flash.h"
-#include "hardware/sync.h"
+  #include "hardware/sync.h"
+  #include "hardware/flash.h"
 };
 
 extern char __flash_binary_end;
 
 #define SAFE_START_ADDR (XIP_BASE + FLASH_TARGET_OFFSET)
-#define MAX_FLASH_ADDR (16 * 1024 * 1024)   // 16MB flash (from XIP_BASE to the end)
-#define BINARY_END_ADDR __flash_binary_end  // XIP_BASE + BINARY_SIZE
+#define MAX_FLASH_ADDR (16 * 1024 * 1024)  // 16MB flash (from XIP_BASE to the end)
+#define BINARY_END_ADDR __flash_binary_end // XIP_BASE + BINARY_SIZE
 
 class Flash {
-public:
-    int findFirstEmptyPageInSector(int start_addr);
-    int findFirstEmptyPage(uint32_t start_addr, uint32_t max_addr);
-    int programSinglePage(int start_addr, uint8_t *data, int size);
-    void erase(int start_addr);
-    void programMultiplePages(int start_addr, uint8_t *data, int size);
-    void read(int start_addr, uint8_t *reading, int nbytes);
+  public:
+    int findFirstEmptyPageInSector(int flash_offs);
+    int findFirstEmptyPage(uint32_t upperLimitOffset, uint32_t lowerLimitOffset);
+    int programSinglePage(int flash_offs, uint8_t* data, int size);
+    void erase(int flash_offs);
+    void programMultiplePages(int flash_offs, uint8_t* data, int size);
+    void read(int flash_offs, uint8_t* reading, int nbytes);
 
-private:
+  private:
 };
--- software/drivers/src/rp_agrolib_flash.cpp
+++ software/drivers/src/rp_agrolib_flash.cpp
@@ -1,88 +1,97 @@
-// See https://www.makermatrix.com/blog/read-and-write-data-with-the-pi-pico-onboard-flash/ for more
-// information about this!
+// See https://www.makermatrix.com/blog/read-and-write-data-with-the-pi-pico-onboard-flash/ for more information about this!
 
 #include "../include/rp_agrolib_flash.h"
 
 /*
  * This function finds the first empty page inside a sector
  */
-int Flash::findFirstEmptyPageInSector(int start_addr) {
-    int page, addr, first_empty_page = -1;
-    int *p;
-    for (page = 0; page < FLASH_SECTOR_SIZE / FLASH_PAGE_SIZE; ++page) {
-        addr = XIP_BASE + start_addr + (page * FLASH_PAGE_SIZE);
-        p = (int *) addr;
-        // 0xFFFFFFFF cast as an int is -1 so this is how we detect an empty page
-        if (*p == -1 && first_empty_page < 0) {
-            first_empty_page = page;
-            break;
-        }
+int Flash::findFirstEmptyPageInSector(int flash_offs)
+{
+  int page, addr, first_empty_page = -1;
+  int* p;
+  for (page = 0; page < FLASH_SECTOR_SIZE / FLASH_PAGE_SIZE; ++page)
+  {
+    addr = XIP_BASE + flash_offs + ((page + 1) * FLASH_PAGE_SIZE);
+    p = (int*) addr;
+    // 0xFFFFFFFF cast as an int is -1 so this is how we detect an empty page
+    if (*p == -1 && first_empty_page < 0)
+    {
+      first_empty_page = page;
+      break;
     }
-    return first_empty_page;
+  }
+  return first_empty_page;
 }
 
 /*
- * This function finds the first empty page
+ * Finds the first empty flash page by searching from the end of the
+ * search range toward the beginning (from upperLimitOffset to lowerLimitOffset).
  * Start: PICO_FLASH_SIZE_BYTES
- * End: __flash_binary_end (XIP_BASE + BINARY_SIZE)
+ * End: __flash_binary_end (XIP_BASE + BINARY_SIZE) - XIP_BASE 
  */
-int Flash::findFirstEmptyPage(uint32_t start_addr, uint32_t max_addr) {
-    int page, addr, first_empty_page = -1;
-    int *p;
+int Flash::findFirstEmptyPage(uint32_t upperLimitOffset, uint32_t lowerLimitOffset)
+{
+  int page, addr, first_empty_page = -1;
+  int* p;
 
-    int max_pages = (start_addr - max_addr) / FLASH_PAGE_SIZE;
-    int pages_per_sector = FLASH_SECTOR_SIZE / FLASH_PAGE_SIZE;
+  int max_pages = (upperLimitOffset - lowerLimitOffset) / FLASH_PAGE_SIZE;
+  int pages_per_sector = FLASH_SECTOR_SIZE / FLASH_PAGE_SIZE;
 
-    for (page = 0; page < max_pages; ++page) {
-        addr = XIP_BASE + start_addr - (page * FLASH_PAGE_SIZE);
-        p = (int *) addr;
-        // 0xFFFFFFFF cast as an int is -1 so this is how we detect an empty page
-        if (*p == -1 && first_empty_page < 0) {
-            first_empty_page = page;
-            break;
-        }
+  for (page = 0; page < max_pages; ++page)
+  {
+    addr = XIP_BASE + upperLimitOffset - ((page + 1) * FLASH_PAGE_SIZE);
+    p = (int*) addr;
+    // 0xFFFFFFFF cast as an int is -1 so this is how we detect an empty page
+    if (*p == -1 && first_empty_page < 0)
+    {
+      first_empty_page = page;
+      break;
     }
-    return first_empty_page;
+  }
+  return first_empty_page;
 }
 
 /*
- * This function programs a single page if possible (if the page is empty) without erasing the
- * entire sector
+ * This function programs a single page if possible (if the page is empty) without erasing the entire sector
  */
-int Flash::programSinglePage(int start_addr, uint8_t *data, int size) {
-    if (size > FLASH_PAGE_SIZE) {
-        printf("Err flash prog single: 'size' bigger than 'FLASH_PAGE_SIZE'\n");
-        return -1;
-    }
+int Flash::programSinglePage(int flash_offs, uint8_t* data, int size)
+{
+  if (size > FLASH_PAGE_SIZE)
+  {
+    printf("Err flash prog single: 'size' bigger than 'FLASH_PAGE_SIZE'\n");
+    return -1;
+  }
 
-    int addr = XIP_BASE + start_addr;
-    int *p = (int *) addr;
-    if (*p != -1) {
-        printf("Err flash prog single: page is not empty\n");
-        return -2;
-    }
+  int addr = XIP_BASE + flash_offs;
+  int* p = (int*) addr;
+  if (*p != -1)
+  {
+    printf("Err flash prog single: page is not empty\n");
+    return -2;
+  }
 
-    printf("Address addr: %d\n", addr);
-    printf("Address start_addr (xip_base + start_addr): %d\n", start_addr);
+  printf("Address addr: %d\n", addr);
+  printf("Address start_addr (xip_base + flash_offs): %d\n", flash_offs);
 
-    uint8_t buf[1 * (FLASH_PAGE_SIZE / sizeof(uint8_t))];
-    for (size_t i = 0; i < size; ++i)
-        buf[i] = data[i];
+  uint8_t buf[1 * (FLASH_PAGE_SIZE / sizeof(uint8_t))];
+  for (size_t i = 0; i < size; ++i)
+    buf[i] = data[i];
 
-    uint32_t interrupts;
-    interrupts = save_and_disable_interrupts();
-    flash_range_program(start_addr, (uint8_t *) buf, FLASH_PAGE_SIZE);
-    restore_interrupts(interrupts);
-    return 0;
+  uint32_t interrupts;
+  interrupts = save_and_disable_interrupts();
+  flash_range_program(flash_offs, (uint8_t*) buf, FLASH_PAGE_SIZE);
+  restore_interrupts(interrupts);
+  return 0;
 }
 
 /*
  * This function erase an entire sector
  */
-void Flash::erase(int start_addr) {
-    uint32_t interrupts = save_and_disable_interrupts();
-    flash_range_erase(start_addr, FLASH_SECTOR_SIZE);
-    restore_interrupts(interrupts);
+void Flash::erase(int flash_offs)
+{
+  uint32_t interrupts = save_and_disable_interrupts();
+  flash_range_erase(flash_offs, FLASH_SECTOR_SIZE);
+  restore_interrupts(interrupts);
 }
 
 // TODO: Missing address validation
@@ -92,29 +101,33 @@ void Flash::erase(int start_addr) {
 /*
  * This function programs and erases multiple pages
  */
-void Flash::programMultiplePages(int start_addr, uint8_t *data, int size) {
-    int multiples_pages = size / FLASH_PAGE_SIZE;
+void Flash::programMultiplePages(int flash_offs, uint8_t* data, int size)
+{
+  int multiples_pages = size / FLASH_PAGE_SIZE;
 
-    uint8_t buf[(multiples_pages + 1) * (FLASH_PAGE_SIZE / sizeof(uint8_t))];
-    for (size_t i = 0; i < size; ++i) {
-        buf[i] = data[i];
-    }
+  uint8_t buf[(multiples_pages + 1) * (FLASH_PAGE_SIZE / sizeof(uint8_t))];
+  for (size_t i = 0; i < size; ++i)
+  {
+    buf[i] = data[i];
+  }
 
-    uint32_t interrupts = save_and_disable_interrupts();
-    flash_range_erase(start_addr, FLASH_SECTOR_SIZE);
-    flash_range_program(start_addr, (uint8_t *) buf, FLASH_PAGE_SIZE);
-    restore_interrupts(interrupts);
+  uint32_t interrupts = save_and_disable_interrupts();
+  flash_range_erase(flash_offs, FLASH_SECTOR_SIZE);
+  flash_range_program(flash_offs, (uint8_t*) buf, FLASH_PAGE_SIZE);
+  restore_interrupts(interrupts);
 }
 
 /*
  * This function read data from flash
  */
-void Flash::read(int start_addr, uint8_t *reading, int nbytes) {
-    uint8_t *p;
-    int start = XIP_BASE + start_addr;
-    for (size_t i = 0; i < nbytes; ++i) {
-        p = (uint8_t *) (start + i);
-        reading[i] = *p;
-    }
-    reading[nbytes] = '\0';
+void Flash::read(int flash_offs, uint8_t* reading, int nbytes)
+{
+  uint8_t *p;
+  int start = XIP_BASE + flash_offs;
+  for (size_t i = 0; i < nbytes; ++i)
+  {
+    p = (uint8_t *)(start+i);
+    reading[i] = *p;
+  }
+  reading[nbytes] = '\0';
 }
